5d671594022f5e8c4ebaf871d8956d01317b3a43
Denise Baker <denviol@yahoo.com>
2019-10-02 18:47:04 -0500

    added fight buttons

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,11 +2,13 @@ import '../styles/main.scss';
 import 'bootstrap';
 import eat from './components/eat';
 import play from './components/play';
+import fight from './components/fight';
 
 
 const init = () => {
   eat.createEatButtons();
   play.createPlayButtons();
+  fight.createFightButtons();
 };
 
 init();
