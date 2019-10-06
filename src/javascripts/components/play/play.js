91dedf37a9d10fc9c1f327ca61027c3d0b892a27
Denise Baker <denviol@yahoo.com>
2019-10-06 18:48:33 -0500

    add play quad buttons functionality

--- src/javascripts/components/play/play.js
+++ src/javascripts/components/play/play.js
@@ -2,20 +2,42 @@ import utilities from '../../helpers/utilities';
 
 import './play.scss';
 
+let fun = 50;
+
+const playVideoGame = () => {
+  let video = fun + 50;
+  if (video > 100) {
+    video = 100;
+  }
+  fun = video;
+  document.getElementById('fun-bar').setAttribute('style', `width: ${fun}%`);
+};
+
+const watchTV = () => {
+  let tv = fun + 2;
+  if (tv > 100) {
+    tv = 100;
+  }
+  fun = tv;
+  document.getElementById('fun-bar').setAttribute('style', `width: ${fun}%`);
+};
+
 const createPlayButtons = () => {
   let domString = '';
   domString += `
   <div class="button-container">
-    <button id='super-fun' class='button'>Super fun!</button>
-    <button id='fun' class='button'>Fun</button>
+    <button id='video-game' class='button'>Play Video Game</button>
+    <button id='tv' class='button'>Watch TV</button>
   </div>
-  <div id="progress-container">
-      <div id="myProgress">
-        <div id="myBar" style="width: 20%"></div>
+  <div id="fun-progress-container">
+      <div id="fun-progress">
+        <div id="fun-bar" style="width: ${fun}%"></div>
       </div>
     </div>
   `;
   utilities.printToDom(domString, 'play');
+  document.getElementById('video-game').addEventListener('click', playVideoGame);
+  document.getElementById('tv').addEventListener('click', watchTV);
 };
 
 export default { createPlayButtons };
